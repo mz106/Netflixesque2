a3adc0eb99a0f4bf775ce019daf220ab2362f6f9
dave franks <flashman123@fastmail.fm>
2021-08-19 15:22:24 +0100

    model Customer created

---
+++ utils/customer.js
@@ -0,0 +1,20 @@
+
+const { DataTypes } = require('sequelize');
+const { connection } = require('./mysql');
+
+const Customer = connection.define("Customer", {
+    id: {type: DataTypes.INTEGER, allowNull: false},
+    name: {type: DataTypes.STRING},
+    movie_id: {type: DataTypes.INTEGER},
+    
+}, {});
+
+const main = async () => {
+    try {
+        await Customer.sync({alter: true});
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+module.exports = Customer;
--- utils/mysql.js
+++ utils/mysql.js
@@ -3,7 +3,7 @@
 
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME_MYSQL, process.env.DB_USER_MYSQL, process.env.DB_PASSWORD_MYSQL, {
+exports.connection = new Sequelize(process.env.DB_NAME_MYSQL, process.env.DB_USER_MYSQL, process.env.DB_PASSWORD_MYSQL, {
     host: process.env.DB_HOST_MYSQL,
     dialect: process.env.DB_DIALECT_MYSQL
 });
