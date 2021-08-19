f2fd3821668065a5066c812ad4b652bf9b36aef0
dave franks <flashman123@fastmail.fm>
2021-08-19 14:57:01 +0100

    mongo db connection name change

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoConnect = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+const mongoConnect = mongoose.connect(`mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME_MONGO}`,
     {useNewUrlParser: true, useUnifiedTopology: true},
 );
 
