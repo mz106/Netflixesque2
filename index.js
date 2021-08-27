282e4d427e9badbbf5cab96efa570ba2998a9443
Michael Z <flashman123@fastmail.fm>
2021-08-27 12:19:41 +0100

    simple user routes added

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const { connection } = require("./db");
 const app = express();
 const port = process.env.PORT;
 const errorRouter = require("./routes/error");
+const userRouter = require("./routes/user");
 
 app.use(express.json());
 
+app.use("/user", userRouter);
 app.use("*", errorRouter);
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,21 @@
 
+const express = require("express");
+
+const router = express.Router();
+
+// register user
+router.post("/register", (req, res) => {
+    res.status(201).send("the user register route is working");
+});
+
+// login user
+router.post("/login", (req, res) => {
+    res.status(200).send("the user login route is working");
+});
+
+// profile user
+router.post("/profile", (req, res) => {
+    res.status(200).send("the user login route is working");
+});
+
+module.exports = router;
