e658f88fccb8f8510f46f37c792548e947564539
Michael Z <flashman123@fastmail.fm>
2021-08-27 12:02:15 +0100

    error route added

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 require("dotenv").config();
-const { connection } = require("./db");
+const express = require("express");
 
+const { connection } = require("./db");
+const app = express();
+const port = process.env.PORT;
+const errorRouter = require("./routes/error");
+
+app.use(express.json());
+
+app.use("*", errorRouter);
+
+
+app.listen(port || 5000, async () => {
+    connection.authenticate();
+    console.log("app is listening")
+});
                                                                                                                                                       
 
 
